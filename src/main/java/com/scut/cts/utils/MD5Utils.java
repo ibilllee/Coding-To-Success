8048723a01d3563747aba57c6d9343f54ca3fcb9
ibilllee <ibilllee@qq.com>
2021-06-05 10:13:21 +0800

    Create MD5Utils

---
+++ src/main/java/com/scut/cts/utils/MD5Utils.java
@@ -0,0 +1,12 @@
+package com.scut.cts.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+public class MD5Utils {
+	public static String EncoderByMd5(String str) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+		return new String(Base64.getEncoder().encode(MessageDigest.getInstance("MD5").digest(str.getBytes("utf-8"))));
+	}
+}
