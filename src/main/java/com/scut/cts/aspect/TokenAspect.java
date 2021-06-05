aa011b6d244d99f945d89d39cf1eec044c6dd2b7
ibilllee <ibilllee@qq.com>
2021-06-05 17:32:41 +0800

    Finish TokenAspect

---
+++ src/main/java/com/scut/cts/aspect/TokenAspect.java
@@ -0,0 +1,39 @@
+package com.scut.cts.aspect;
+
+import com.scut.cts.utils.TokenUtils;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Set;
+
+@Aspect
+@Component
+public class TokenAspect
+{
+	private static Set<String> noTokenMethods = new HashSet<String>()
+	{
+		// add("");
+	};
+
+
+	@Before("execution(* com.scut.cts.controller.*.*(..))")
+	public void checkToken(JoinPoint joinPoint) throws Throwable {
+		/*String methodName = joinPoint.getSignature().getName();
+		if (!noTokenMethods.contains(methodName)) {
+			String token = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest().getHeader("token");
+			if(!TokenUtils.verify(token)){
+				throw new HttpServerErrorException(HttpStatus.UNAUTHORIZED, "Your token is invalid");
+			}
+		}*/
+	}
+}
