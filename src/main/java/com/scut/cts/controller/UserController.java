38ecb84754d2012cdeb74a7a679d06f54f305368
AnkleBreaker-ACM <yf18318415606@126.com>
2021-06-05 22:01:45 +0800

    '新增UserController'

--- src/main/java/com/scut/cts/controller/UserController.java
+++ src/main/java/com/scut/cts/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.scut.cts.controller;
 
 import com.scut.cts.pojo.RespBean;
+import com.scut.cts.pojo.User;
 import com.scut.cts.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -9,5 +10,51 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/user")
 public class UserController
 {
+    @Autowired
+    private UserService userService;
 
+    @PostMapping("/register")
+    public RespBean register(@RequestParam String userId, @RequestParam String password) {
+        User user = new User(userId,password);
+        boolean result;
+        try {
+            result = userService.register(user);
+        }catch (Exception e) {
+            return RespBean.unprocessable("注册失败"+e.getMessage());
+        }
+        if(result) {
+            return RespBean.created("注册成功",user);
+        }
+        return RespBean.unprocessable("注册失败");
+    }
+
+    @PostMapping("/login")
+    public RespBean login(@RequestParam String userId, @RequestParam String password) {
+        User user = new User(userId, password);
+        boolean result;
+        try {
+            result = userService.login(user);
+        }catch (Exception e) {
+            return RespBean.unprocessable("用户登录失败"+e.getMessage());
+        }
+        if(result) {
+            return RespBean.ok("用户登录成功",user);
+        }
+        return RespBean.unprocessable("用户登录失败");
+    }
+
+    @PutMapping("/{userId}")
+    public RespBean updateUser(@RequestParam String userId, @RequestParam String password) {
+        User user = new User(userId, password);
+        boolean result;
+        try {
+            result = userService.updateUser(user);
+        }catch (Exception e) {
+            return RespBean.unprocessable("用户密码修改失败"+e.getMessage(),user);
+        }
+        if(result) {
+            return RespBean.ok("用户密码修改成功",user);
+        }
+        return RespBean.unprocessable("用户密码修改失败",user);
+    }
 }
