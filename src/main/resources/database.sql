77ad6fb42c86056bdce0ba35832490cedb2fffef
ibilllee <ibilllee@qq.com>
2021-06-08 11:34:08 +0800

    Update database.sql

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -1,5 +1,5 @@
 DROP DATABASE cts;
-CREATE DATABASE IF NOT EXISTS  cts DEFAULT CHARSET utf8mb4;
+CREATE DATABASE IF NOT EXISTS cts DEFAULT CHARSET utf8mb4;
 USE cts;
 
 DROP TABLE IF EXISTS bulletin;
@@ -10,73 +10,92 @@ DROP TABLE IF EXISTS tutorial;
 DROP TABLE IF EXISTS admin;
 DROP TABLE IF EXISTS USER;
 
-CREATE TABLE IF NOT EXISTS  USER(
-    user_id VARCHAR(20) PRIMARY KEY,
-    PASSWORD VARCHAR(255) NOT NULL,
-    STATUS BOOL,
-    head_address TEXT NOT NULL);
+CREATE TABLE IF NOT EXISTS USER
+(
+    user_id      VARCHAR(20) PRIMARY KEY,
+    PASSWORD     VARCHAR(255) NOT NULL,
+    STATUS       BOOL,
+    head_address TEXT         NOT NULL
+);
 
-CREATE TABLE IF NOT EXISTS  admin(
+CREATE TABLE IF NOT EXISTS admin
+(
     admin_id VARCHAR(20) PRIMARY KEY,
-    PASSWORD VARCHAR(20) NOT NULL);
+    PASSWORD VARCHAR(20) NOT NULL
+);
 
-CREATE TABLE IF NOT EXISTS  tutorial(
-    tuto_id INT PRIMARY KEY auto_increment,
+CREATE TABLE IF NOT EXISTS tutorial
+(
+    tuto_id       INT PRIMARY KEY auto_increment,
     title_content VARCHAR(255),
-    title_num DOUBLE NOT NULL UNIQUE,
-    content LONGTEXT);
+    title_num     DOUBLE NOT NULL UNIQUE,
+    content       LONGTEXT
+);
 
-CREATE TABLE IF NOT EXISTS  COMMENT(
-    comment_id INT AUTO_INCREMENT PRIMARY KEY,
-    comment_tuto_id INT NOT NULL,
+CREATE TABLE IF NOT EXISTS COMMENT
+(
+    comment_id      INT AUTO_INCREMENT PRIMARY KEY,
+    comment_tuto_id INT         NOT NULL,
     comment_user_id VARCHAR(20) NOT NULL,
-    content TEXT,
-    reply_to INT,
-    reply_order INT,
+    content         TEXT,
+    reply_to        INT,
+    reply_order     INT,
 
     FOREIGN KEY (comment_tuto_id)
-    REFERENCES tutorial(tuto_id)
-    ON DELETE RESTRICT
-    ON UPDATE RESTRICT,
+        REFERENCES tutorial (tuto_id)
+        ON DELETE RESTRICT
+        ON UPDATE RESTRICT,
 
     FOREIGN KEY (comment_user_id)
-    REFERENCES USER(user_id)
-    ON DELETE RESTRICT
-    ON UPDATE RESTRICT
-    );
+        REFERENCES USER (user_id)
+        ON DELETE RESTRICT
+        ON UPDATE RESTRICT
+);
 
-CREATE TABLE IF NOT EXISTS  problem(
-    prob_id INT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS problem
+(
+    prob_id      INT PRIMARY KEY,
     prob_tuto_id INT NOT NULL,
-    content TEXT,
+    content      TEXT,
 
     FOREIGN KEY (prob_tuto_id)
-    REFERENCES tutorial(tuto_id)
-    ON DELETE RESTRICT
-    ON UPDATE RESTRICT
-    );
+        REFERENCES tutorial (tuto_id)
+        ON DELETE RESTRICT
+        ON UPDATE RESTRICT
+);
 
-CREATE TABLE IF NOT EXISTS answer(
+CREATE TABLE IF NOT EXISTS answer
+(
     ans_prob_id INT PRIMARY KEY,
-    content TEXT,
+    content     TEXT,
 
     FOREIGN KEY (ans_prob_id)
-    REFERENCES problem(prob_id)
-    ON DELETE RESTRICT
-    ON UPDATE RESTRICT
-    );
+        REFERENCES problem (prob_id)
+        ON DELETE RESTRICT
+        ON UPDATE RESTRICT
+);
 
-CREATE TABLE IF NOT EXISTS  bulletin(
+CREATE TABLE IF NOT EXISTS bulletin
+(
     bulletin_id INT AUTO_INCREMENT PRIMARY KEY,
-    content TEXT);
+    content     TEXT
+);
 
-INSERT INTO USER VALUES("user","user",TRUE,"adress");
-INSERT INTO tutorial VALUES(1,"标题",1.0,"内容");
-INSERT INTO problem VALUES(1,1,"内容");
-INSERT INTO answer VALUES(1,"内容");
-INSERT INTO COMMENT VALUES(NULL ,1,"user","内容1",NULL,NULL);
-INSERT INTO COMMENT VALUES(NULL ,1,"user","内容2",2,1);
+INSERT INTO USER
+VALUES ("user", "user", TRUE, "adress");
+INSERT INTO tutorial
+VALUES (1, "标题", 1.0, "内容");
+INSERT INTO problem
+VALUES (1, 1, "内容");
+INSERT INTO answer
+VALUES (1, "内容");
+INSERT INTO COMMENT
+VALUES (NULL, 1, "user", "内容1", NULL, NULL);
+INSERT INTO COMMENT
+VALUES (NULL, 1, "user", "内容2", 2, 1);
 
-INSERT INTO ADMIN VALUES("admin","ISMvKXpXpadDiUoOSoAfww==");
+INSERT INTO ADMIN
+VALUES ("admin", "ISMvKXpXpadDiUoOSoAfww==");
 
-INSERT INTO USER VALUES ("CLOSED_ACCOUNT","CLOSED_ACCOUNT",3,"NULL");
+INSERT INTO USER
+VALUES ("CLOSED_ACCOUNT", "CLOSED_ACCOUNT", 3, "NULL");
