b48009476bc7bd57fd9a5c4d1e39e1b4f24bb186
ibilllee <ibilllee@qq.com>
2021-06-10 12:06:06 +0800

    Pack project

--- src/main/java/com/scut/cts/CtsApplication.java
+++ src/main/java/com/scut/cts/CtsApplication.java
@@ -3,11 +3,20 @@ package com.scut.cts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import tk.mybatis.spring.annotation.MapperScan;
 
+@EnableScheduling
 @SpringBootApplication
 @MapperScan("com.scut.cts.mapper")
-public class CtsApplication {
+public class CtsApplication extends SpringBootServletInitializer
+{
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(CtsApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CtsApplication.class, args);
