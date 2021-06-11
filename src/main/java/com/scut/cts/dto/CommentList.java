a375cf4584f4516ce37d9fdc6195a22048862077
ibilllee <ibilllee@qq.com>
2021-06-11 16:25:50 +0800

    Adjust API

--- src/main/java/com/scut/cts/dto/CommentList.java
+++ src/main/java/com/scut/cts/dto/CommentList.java
@@ -1,9 +1,11 @@
 package com.scut.cts.dto;
 
 import com.scut.cts.pojo.Comment;
+import lombok.Data;
 
 import java.util.List;
 
+@Data
 public class CommentList
 {
 	private List<CommentWithAvatar> commentList;
